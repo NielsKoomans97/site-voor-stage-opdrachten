36b0e61783d54ad454955ddcaf61acb1fb03f17b
Niels <44090733+NielsKoomans97@users.noreply.github.com>
2023-02-22 12:40:33 +0100

    Javascript versie van fizz buzz toegevoegd
    De statische versie is vervangen door een live web-versie

--- js/romandigits.js
+++ js/romandigits.js
@@ -13,39 +13,6 @@ function romanize(num) {
     return roman;
   }
 
-// const intToRoman = (num) => {
-//     const map = {
-//       M: 1000,
-//       CM: 900,
-//       D: 500,
-//       CD: 400,
-//       C: 100,
-//       XC: 90,
-//       L: 50,
-//       XL: 40,
-//       X: 10,
-//       IX: 9,
-//       V: 5,
-//       IV: 4,
-//       I: 1,
-//     };
-//     let result = '';
-
-//     for (key in map) {
-//       const repeatCounter = Math.floor(num / map[key]);
-
-//       if (repeatCounter !== 0) {
-//         result += key.repeat(repeatCounter);
-//       }
-
-//       num %= map[key];
-
-//       if (num === 0) return result;
-//     }
-
-//     return result;
-//   };
-
 convertbtn.addEventListener("click", function() {
     let roman = romanize(parseInt(input.value));
 
--- opdracht_2.php
+++ opdracht_2.php
@@ -30,10 +30,9 @@
         <div class="pagepart pagepart-left-container">
             <div class="textalign-left-container">
                 <p class="container-title">Input</p>
-                <span>
-                    De input die ik gegeven heb zijn
-                    <b>X = 3</b> en <b>Y = 5</b>
-                </span>
+                <input type="number" id="xnum">
+                <input type="number" id="ynum">
+                <button id="fizzbuzz">Fizz Buzz</button>
             </div>
             <img class="arrow-icon-reversed" src="img/output.png">
         </div>
@@ -41,14 +40,51 @@
             <img class="vraagteken" src="img/output.png">
             <div class="textalign-right-container">
                 <p class="container-title">Output</p>
-                <span>
-                    <?php
-                        $_filename = "./opdracht2/output.txt";
-                        $_contents = file_get_contents($_filename);
+                <span id="output" class="output">
 
-                        echo nl2br($_contents);
-                    ?>
                 </span>
+
+                <script>
+                    let fizzbuzz = document.getElementById("fizzbuzz");
+
+                    fizzbuzz.addEventListener("click", function(){
+                        let xnum = parseInt(document.getElementById("xnum").value);
+                        let ynum = parseInt(document.getElementById("ynum").value);
+                        let output = document.getElementById("output");
+                        let out = "";
+
+                        for(let i = 0; i < 100; i++)
+                        {
+                            if (i % xnum == 0){
+                                if (i % ynum == 0){
+                                    out += "FB";
+                                    console.log("FB");
+                                }
+                                else
+                                {
+                                    out += "F";
+                                    console.log("F");
+                                }
+                            }
+                            else if(i % ynum == 0){
+                                if (i % xnum == 0){
+                                    out += "FB";
+                                    console.log("FB");
+                                }
+                                else{
+                                    out += "B";
+                                    console.log("B");
+                                }
+                            }
+                            else{
+                                out += i.toString();
+                                console.log(i.toString());
+                            }
+                        }
+
+                        output.innerText = out;
+                    });
+                </script>
             </div>
         </div>
 
